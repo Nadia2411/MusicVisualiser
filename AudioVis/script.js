508ee8bc2b86e36db849ee4bc2b575cb702abcb7
Nadia2411 <57686307+Nadia2411@users.noreply.github.com>
2023-08-10 15:29:19 +0100

    colour mode

--- AudioVis/script.js
+++ AudioVis/script.js
@@ -2,124 +2,73 @@ let song;
 let fft;
 let amplitude;
 let cnv;
+let visualizerColor = '#FFFFFF';
+let backgroundColor = '#000000';
 
 function setup() {
-  if (getAudioContext().state !== 'running') {
-    getAudioContext().resume();
-  }
-
   cnv = createCanvas(600, 400);
   cnv.parent('canvasContainer');
   fft = new p5.FFT();
   amplitude = new p5.Amplitude();
   noLoop();
   document.getElementById('playButton').disabled = true;
+  background(backgroundColor);
 }
 
-let visualizationStarted = false;
 function playMusic() {
-  let btn = document.querySelector('button');
+  let btn = document.getElementById('playButton');
   if (song && song.isPlaying()) {
     song.pause();
     btn.innerText = 'Play Music';
     noLoop();
-    visualizationStarted = false;
-  } else {
-    if (song) {
-      song.play();
-      btn.innerText = 'Pause Music';
-
-      setTimeout(() => {
-        visualizationStarted = true;
-        loop();
-      }, 100);
-    }
-  }
-}
-
-let btn = document.querySelector('button');
-if (song && song.isPlaying()) {
-  song.pause();
-  btn.innerText = 'Play Music';
-} else {
-  if (song) {
+  } else if (song) {
     song.play();
     btn.innerText = 'Pause Music';
+    loop();
   }
 }
 
 function draw() {
-  if (!visualizationStarted) return;
-  clear();
-
-  let bgColor = color(document.getElementById('bgColor').value);
-  let primaryColor = color(document.getElementById('primaryColor').value);
-  let shape = document.getElementById('shapeSelector').value;
+  background(backgroundColor);
+  stroke(visualizerColor);
   let bassBoost = parseFloat(document.getElementById('bassBoost').value);
-
-  background(bgColor);
+  let spectrum = fft.analyze();
+  let shape = document.getElementById('shapeSelector').value;
 
   switch (shape) {
     case 'bars':
-      drawBars(primaryColor, bassBoost);
+      drawBars(spectrum, bassBoost);
       break;
     case 'circles':
-      drawCircles(primaryColor, bassBoost);
-      break;
-
-    case 'particles':
-      drawParticles(primaryColor, bassBoost);
-      break;
-    case 'spiral':
-      drawSpiral(primaryColor, bassBoost);
-      break;
-    case 'concentricCircles':
-      drawConcentricCircles(primaryColor, bassBoost);
+      drawCircles(spectrum, bassBoost);
       break;
     case 'waves':
-      drawWaves(primaryColor, bassBoost);
+      drawWaves(spectrum, bassBoost);
       break;
   }
 }
 
-function drawBars(primaryColor, bassBoost) {
-  let spectrum = fft.analyze();
-  noStroke();
-
-  let baseColor = color(document.getElementById('barBaseColor').value);
-  let tipColor = color(document.getElementById('barTipColor').value);
-
+function drawBars(spectrum, bassBoost) {
   for (let i = 0; i < spectrum.length; i++) {
-    let interColor = lerpColor(baseColor, tipColor, spectrum[i] / 255.0);
-    fill(interColor);
     let x = map(i, 0, spectrum.length, 0, width);
-
-    let barWidth = parseFloat(document.getElementById('barThickness').value);
-    let barSpacing = parseFloat(document.getElementById('barSpacing').value);
-
     let h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x * barSpacing, height, barWidth, h * bassBoost);
+    rect(x, height, width / spectrum.length, h * bassBoost);
   }
 }
 
-function drawCircles(primaryColor, bassBoost) {
-  let lvl = amplitude.getLevel();
-  let size = map(lvl, 0, 1, 0, 200) * bassBoost;
-
-  let baseColor = color(document.getElementById('barBaseColor').value);
-  let tipColor = color(document.getElementById('barTipColor').value);
-
-  ellipse(width / 2, height / 2, size, size);
+function drawCircles(spectrum, bassBoost) {
+  fill(visualizerColor);
+  let r = map(spectrum[0], 0, 255, 10, width / 4) * bassBoost;
+  ellipse(width / 2, height / 2, r);
 }
 
-function drawWaves(primaryColor, bassBoost) {
-  let waveform = fft.waveform();
+function drawWaves(spectrum, bassBoost) {
+  let waveformData = fft.waveform();
   noFill();
   beginShape();
-  stroke(primaryColor);
-  for (let i = 0; i < waveform.length; i++) {
-    let x = map(i, 0, waveform.length, 0, width);
-    let y = map(waveform[i] * bassBoost, -1, 1, 0, height);
+  for (let i = 0; i < waveformData.length; i++) {
+    let x = map(i, 0, waveformData.length, 0, width);
+    let y = map(waveformData[i] * bassBoost, -1, 1, 0, height);
     vertex(x, y);
   }
   endShape();
@@ -136,87 +85,27 @@ document
     });
   });
 
-function drawConcentricCircles(primaryColor, bassBoost) {
-  let spectrum = fft.analyze();
-  let bands = spectrum.length / 10;
-  for (let i = 0; i < bands; i++) {
-    let avg = 0;
-    for (let j = 0; j < 10; j++) {
-      avg += spectrum[i * 10 + j];
-    }
-    avg /= 10;
-    let radius = map(avg, 0, 255, 10, height / 2) * bassBoost;
-    noFill();
-    stroke(primaryColor);
-    ellipse(width / 2, height / 2, radius, radius);
-  }
+function setDefaultMode() {
+  visualizerColor = '#FFFFFF';
+  backgroundColor = '#000000';
+  background(backgroundColor);
 }
 
-function drawSpiral(primaryColor, bassBoost) {
-  let spectrum = fft.analyze();
-  translate(width / 2, height / 2);
-  for (let i = 0; i < spectrum.length; i++) {
-    let angle = map(i, 0, spectrum.length, 0, TWO_PI * 5);
-    let r = map(i, 0, spectrum.length, 20, height / 2);
-    let x = r * cos(angle);
-    let y = r * sin(angle);
-    let h = -height + map(spectrum[i], 0, 255, height, 0);
-    let w = map(spectrum[i], 0, 255, 0, 10);
-    push();
-    translate(x, y);
-    rotate(angle + HALF_PI);
-    fill(primaryColor);
-    rect(0, 0, w, h * bassBoost);
-    pop();
-  }
+function setColorMode() {
+  visualizerColor = '#FF0000'; // Example color, can be changed
+  backgroundColor = '#0000FF'; // Example color, can be changed
+  background(backgroundColor);
 }
 
-let particles = [];
+let isDefaultMode = true;
 
-function drawParticles(primaryColor, bassBoost) {
-  let spectrum = fft.analyze();
-  let bassValue = fft.getEnergy('bass');
-
-  // Add new particles based on bass value
-  for (let i = 0; i < bassValue / 10; i++) {
-    particles.push(new Particle(primaryColor));
+function toggleMode() {
+  if (isDefaultMode) {
+    setColorMode();
+    document.getElementById('modeToggleButton').innerText = 'Color Mode';
+  } else {
+    setDefaultMode();
+    document.getElementById('modeToggleButton').innerText = 'Default Mode';
   }
-
-  // Update and display particles
-  for (let i = particles.length - 1; i >= 0; i--) {
-    particles[i].update(bassBoost);
-    particles[i].display(spectrum);
-    if (particles[i].isOffScreen()) {
-      particles.splice(i, 1);
-    }
-  }
-}
-
-class Particle {
-  constructor(primaryColor) {
-    this.pos = createVector(width / 2, height / 2);
-    this.vel = createVector(random(-1, 1), random(-1, 1)).mult(random(2, 5));
-    this.size = random(2, 10);
-    this.color = primaryColor;
-  }
-
-  update(bassBoost) {
-    this.vel.mult(bassBoost);
-    this.pos.add(this.vel);
-  }
-
-  display(spectrum) {
-    noStroke();
-    fill(this.color);
-    ellipse(this.pos.x, this.pos.y, this.size, this.size);
-  }
-
-  isOffScreen() {
-    return (
-      this.pos.x < 0 ||
-      this.pos.x > width ||
-      this.pos.y < 0 ||
-      this.pos.y > height
-    );
-  }
+  isDefaultMode = !isDefaultMode;
 }
